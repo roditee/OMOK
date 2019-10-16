d79c94b7210d9756f2b1c4dacbcd2ab89a2138ba
roditee <56589920+roditee@users.noreply.github.com>
2019-10-16 21:50:07 +0900

    Upload 오목 file

---
+++ 오목/오목/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace 오목
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.메뉴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.새게임ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.메뉴ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(381, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 메뉴ToolStripMenuItem
+            // 
+            this.메뉴ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.새게임ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.메뉴ToolStripMenuItem.Name = "메뉴ToolStripMenuItem";
+            this.메뉴ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.메뉴ToolStripMenuItem.Text = "메뉴";
+            // 
+            // 새게임ToolStripMenuItem
+            // 
+            this.새게임ToolStripMenuItem.Name = "새게임ToolStripMenuItem";
+            this.새게임ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.새게임ToolStripMenuItem.Text = "새 게임";
+            this.새게임ToolStripMenuItem.Click += new System.EventHandler(this.새게임ToolStripMenuItem_Click_1);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 402);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "오목 게임";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 새게임ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+    }
+}
+
---
+++ 오목/오목/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 오목
+{
+    public partial class Form1 : Form
+    {
+        private int 돌size = 18;
+        private int 눈size = 20;
+        private int 화점size = 8;
+        private Pen pen;
+        private Brush wBrush, bBrush;
+
+        private bool turn = false; // false: 흑돌, true: 백돌
+        enum STONE { none, black, white };  //none 바둑돌이없다, black 검은돌이있다, white 흰 돌이 있다
+        STONE[,] 바둑판 = new STONE[19, 19]; 
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Black);
+            wBrush = new SolidBrush(Color.White);
+            bBrush = new SolidBrush(Color.Black);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            draw바둑판();
+        }
+
+        private void draw바둑판()
+        {
+            Graphics g = panel1.CreateGraphics();
+            for (int x = 0; x < 19; x++)
+                g.DrawLine(pen, 10, 10 + x * 눈size, 370, 10 + x * 눈size);
+            for (int x = 0; x < 19; x++)
+                g.DrawLine(pen, 10 + x * 눈size, 10, 10 + x * 눈size, 10 + 18 * 눈size);
+            draw화점(g);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+            int x, y;
+            x = e.X / 눈size;
+            y = e.Y / 눈size;
+            if (x < 0 || x >= 19 || y < 0 || y >= 19)
+                return;
+            draw돌(x, y);
+        }
+        private void draw돌(int x, int y)
+        {
+            Graphics g = panel1.CreateGraphics();
+
+            // 이미 어떤 돌이 놓여져 있으면 return
+            if (바둑판[x, y] == STONE.black || 바둑판[x, y] == STONE.white)
+                return;
+
+            Rectangle r = new Rectangle(10 + 눈size * x - 돌size / 2,
+                 10 + 눈size * y - 돌size / 2, 돌size, 돌size);
+
+            if (turn == false)    // 검은 돌
+            {
+                g.FillEllipse(bBrush, r);
+                //Bitmap bmp = new Bitmap("../../images/black.png");
+                //g.DrawImage(bmp, r);
+                바둑판[x, y] = STONE.black;
+            }
+            else  // 흰돌
+            {
+                g.FillEllipse(wBrush, r);
+                //Bitmap bmp = new Bitmap("../../images/white.png");
+                //g.DrawImage(bmp, r);
+                바둑판[x, y] = STONE.white;
+            }
+            turn = !turn;  // 돌 색깔을 토글
+            checkOmok(x, y);  // 오목이 만들어졌는지 체크하는 함수
+        }
+
+        private void checkOmok(int x, int y)
+        {
+            if (checkLR(x, y) >= 5)
+                MessageBox.Show(바둑판[x, y] + " wins");
+            if (checkUD(x, y) >= 5)
+                MessageBox.Show(바둑판[x, y] + " wins");
+            if (checkSLASH(x, y) >= 5)
+                MessageBox.Show(바둑판[x, y] + " wins");
+            if (checkBACKSLASH(x, y) >= 5)
+                MessageBox.Show(바둑판[x, y] + " wins");
+        }
+
+        private int checkLR(int x, int y)
+        {
+            int cnt = 1;
+            for (int i = 1; i < 5; i++)
+                if (x + i <= 18 && 바둑판[x + i, y] == 바둑판[x, y])
+                    cnt++;
+                else
+                    break;
+            for (int i = 1; i < 5; i++)
+                if (x - i >= 0 && 바둑판[x - i, y] == 바둑판[x, y])
+                    cnt++;
+                else
+                    break;
+            return cnt;
+        }
+
+        private int checkUD(int x, int y)
+        {
+            int cnt = 1;
+            for (int i = 1; i < 5; i++)
+                if (x + i <= 18 && 바둑판[x + i, y] == 바둑판[x, y])
+                    cnt++;
+                else
+                    break;
+            for (int i = 1; i < 5; i++)
+                if (x - i >= 0 && 바둑판[x - i, y] == 바둑판[x, y])
+                    cnt++;
+                else
+                    break;
+            return cnt;
+        }
+
+        private int checkSLASH(int x, int y)
+        {
+            int cnt = 1;
+            for (int i = 1; i < 5; i++)
+                if (x + i <= 18 && 바둑판[x + i, y] == 바둑판[x, y])
+                    cnt++;
+                else
+                    break;
+            for (int i = 1; i < 5; i++)
+                if (x - i >= 0 && 바둑판[x - i, y] == 바둑판[x, y])
+                    cnt++;
+                else
+                    break;
+            return cnt;
+        }
+
+        private int checkBACKSLASH(int x, int y)
+        {
+            int cnt = 1;
+            for (int i = 1; i < 5; i++)
+                if (x + i <= 18 && 바둑판[x + i, y] == 바둑판[x, y])
+                    cnt++;
+                else
+                    break;
+            for (int i = 1; i < 5; i++)
+                if (x - i >= 0 && 바둑판[x - i, y] == 바둑판[x, y])
+                    cnt++;
+                else
+                    break;
+            return cnt;
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void 새게임ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 19; x++)
+                for (int y = 0; y < 19; y++)
+                    바둑판[x, y] = STONE.none;
+            turn = false;
+            Refresh();
+            draw바둑판();
+        }
+
+        private void 새게임ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 19; x++)
+                for (int y = 0; y < 19; y++)
+                    바둑판[x, y] = STONE.none;
+            turn = false;
+            Refresh();
+            draw바둑판();
+        }
+
+        private void 종료ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void draw화점(Graphics g)
+        {
+            for (int x = 3; x <= 15; x += 6)
+                for (int y = 3; y <= 15; y += 6)
+                {
+                    Rectangle r = new Rectangle(10 + 눈size * x - 화점size / 2, 10 + 눈size * y - 화점size / 2, 화점size, 화점size);
+                    g.FillEllipse(bBrush, r);
+                }
+        }
+        
+       
+    }
+}
